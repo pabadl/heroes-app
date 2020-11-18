3b798fa684d5fd83e117b57ad4c63707a3b6eb64
Juan Pablo Abad Lopez <pabadl@pabadl.local>
2020-11-17 23:30:52 -0500

    Create Project structure and set webpack config

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [ CommonModule, BrowserModule ],
+    exports: [],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
